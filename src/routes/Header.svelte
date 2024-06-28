3346042ead6bab40e88ee42d251841e71f1d1206
hareubi <70735563+hareubi@users.noreply.github.com>
2024-06-28 23:56:21 +0900

    added picocss

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,67 +1,43 @@
 <script>
 	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
-	import github from '$lib/images/github.svg';
+	const items = [{text: 'Home', link: "/"},
+                    {text: 'Sverdle', link: "/sverdle"},
+                    {text: 'about', link: "/about"},
+					{text: 'board', link: "/board"},
+					{text: 'plan', link: "/plan"}
+					];
 </script>
 
 <header>
-	<div class="corner">
-		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
-		</a>
-	</div>
 
 	<nav>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
 		</svg>
 		<ul>
-			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
-			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
-			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
-			</li>
+			{#each items as item }
+				  <li aria-current={$page.url.pathname === item.link ? 'page' : undefined}>
+					<a href={item.link}>{item.text}</a>
+				</li>
+				{:else}
+					<p>No items available!</p>
+			{/each}
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
 		</svg>
 	</nav>
 
-	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
-			<img src={github} alt="GitHub" />
-		</a>
-	</div>
 </header>
 
 <style>
 	header {
 		display: flex;
 		justify-content: space-between;
-	}
-
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
-		display: flex;
 		align-items: center;
 		justify-content: center;
-		width: 100%;
-		height: 100%;
 	}
 
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
-	}
 
 	nav {
 		display: flex;
---
+++ src/routes/board/+page.svelte
@@ -0,0 +1,35 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<script>
+	const items = [{text: 'Home', link: "/"},
+				{text: 'Sverdle', link: "/sverdle"},
+				{text: 'about', link: "/about"},
+				{text: 'board', link: "/board"},
+				{text: 'plan', link: "/plan"}
+				];
+</script>
+
+<div class="text-column">
+<ol>
+	<ul>
+		{#each items as item }
+				<a href={item.link}>{item.text}</a>
+				<ol>
+					{#each items as item }
+							<a href={item.link}>{item.text}</a>
+						{:else}
+							<p>No items available!</p>
+							
+					{/each}
+				</ol>
+			{:else}
+				<p>No items available!</p>
+
+		{/each}
+	</ul>
+</ol>
+
+</div>
