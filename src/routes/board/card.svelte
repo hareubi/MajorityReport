350216f68dca4aa98804c9d7ab207d3746f79062
hareubi <70735563+hareubi@users.noreply.github.com>
2024-10-03 23:08:55 +0900

    Implemented Kanban Board

---
+++ src/routes/board/card.svelte
@@ -0,0 +1,34 @@
+<script>
+    export let carddata
+</script>
+
+{#if carddata.length > 0}
+    <ul>
+        {#each carddata as card}
+            <li class="cards">
+               {card.title}
+            </li>
+        {/each}
+    </ul>
+    {:else}
+        <p>No Cards...</p>
+{/if}
+<style>
+    ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+        display: flexbox;
+        gap: 1rem;
+    }
+    .cards {
+        flex-direction: row;
+        }
+        li {
+        padding: 1rem;
+        background-color: var(--sk-back-1);
+        border: 1px solid black;
+        border-radius: 0.5rem;
+        border-color: var(--sk-back-5);
+    }
+</style>
