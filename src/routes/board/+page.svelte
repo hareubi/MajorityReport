a43139411f6fe692664966d348fb4145c5a82780
hareubi <indienewbies@gmail.com>
2024-10-03 23:08:55 +0900

    Implemented Kanban Board

--- src/routes/board/+page.svelte
+++ src/routes/board/+page.svelte
@@ -1,35 +1,18 @@
-<svelte:head>
-	<title>About</title>
-	<meta name="description" content="About this app" />
-</svelte:head>
-
 <script>
-	const items = [{text: 'Home', link: "/"},
-				{text: 'Sverdle', link: "/sverdle"},
-				{text: 'about', link: "/about"},
-				{text: 'board', link: "/board"},
-				{text: 'plan', link: "/plan"}
-				];
+	import Cardlist from './cardlist.svelte';
+	export let data
 </script>
 
-<div class="text-column">
-<ol>
-	<ul>
-		{#each items as item }
-				<a href={item.link}>{item.text}</a>
-				<ol>
-					{#each items as item }
-							<a href={item.link}>{item.text}</a>
-						{:else}
-							<p>No items available!</p>
-							
-					{/each}
-				</ol>
-			{:else}
-				<p>No items available!</p>
+<ul>
+    <Cardlist data = {data} />
+</ul>
 
-		{/each}
-	</ul>
-</ol>
-
-</div>
+<style>
+        ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        gap: 1rem;
+    }
+</style>
--- src/routes/board/+page.ts
+++ src/routes/board/+page.ts
@@ -1,9 +1,66 @@
 import { dev } from '$app/environment';
 
-// we don't need any JS on this page, though we'll load
-// it in dev so that we get hot module replacement
 export const csr = dev;
 
-// since there's no dynamic data here, we can prerender
-// it so that it gets served as a static asset in production
-export const prerender = true;
+export const load = async () => {
+	return {
+		rows:[
+			{
+			id: 1,
+			label: 'meow?'
+		},
+		{
+			id: 2,
+			label: 'woof!'
+		},
+	],
+		columns: [
+			{
+				id: 1,
+				row:1,
+				label: '📫 Todo'
+			},
+			{
+				id: 2,
+				row:1,
+				label: '✅ Done'
+			},
+			{
+				id: 1,
+				row:2,
+				label: '📫 Todo1'
+			},
+			{
+				id: 2,
+				row:2,
+				label: '✅ Done1'
+			}
+		],
+		cards: [
+			{
+				row: 1,
+				column: 1,
+				id: 'a',
+				title: 'Wash Dishes'
+			},
+			{
+				row: 1,
+				column: 2,
+				id: 'b',
+				title: 'Code DND Example'
+			},
+			{
+				row: 1,
+				column: 1,
+				id: 'a',
+				title: 'do something'
+			},
+			{
+				row: 1,
+				column: 2,
+				id: 'b',
+				title: 'Code DND Example'
+			}
+		]
+	}
+};
