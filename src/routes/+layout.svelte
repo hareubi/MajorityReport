d156a4e435e9264760568b69c7bc954484fd0587
hareubi <indienewbies@gmail.com>
2024-10-20 23:25:00 +0900

    Deleted counter

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script>
 	import Header from './Header.svelte';
 	import './styles.css';
-	import github from '$lib/images/github.svg';
 </script>
 
 <div class="app">
@@ -11,14 +10,6 @@
 		<slot />
 	</main>
 
-	<footer>
-		<p>meow</p>
-		<div class="corner">
-			<a href="https://github.com/sveltejs/kit">
-				<img src={github} alt="GitHub" />
-			</a>
-		</div>
-	</footer>
 </div>
 
 
@@ -40,40 +31,5 @@
 		margin: 0 auto;
 		box-sizing: border-box;
 	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
 	
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
-		width: 100%;
-		height: 100%;
-		position:fixed;
-		left: 95%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
-	}
 </style>
