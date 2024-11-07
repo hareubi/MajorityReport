55ffec15ef970531daa5e81a83d8440c88a98cf7
null <null>
2024-11-07 22:56:14 +0900

    Implemented navigation bar

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,15 +1,116 @@
-import { Outlet } from "react-router";
+import { Navigate, Outlet, useNavigate } from "react-router";
 import { auth } from "../firebase";
-function logOut(): void {
-  auth.signOut();
-}
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const Wrapper = styled.div`
+  display: grid;
+  gap: 20px;
+  grid-template-columns: 1fr 9fr;
+  padding: 25px 0px;
+  width: 100%;
+`;
+const Menu = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+`;
+const MenuItem = styled.div`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 2px solid black;
+  min-width: 50px;
+  max-width: 100px;
+  border-radius: 30%;
+  border-color: #6c71c4;
+  &.logOut {
+    border-color: #dc322f;
+  }
+`;
 export default function Layout() {
+  const navigate = useNavigate();
+  async function logOut(): void {
+    if (confirm("Logout?")) {
+      await auth.signOut();
+
+      navigate("/join");
+    }
+  }
   return (
     <>
-      <h1>
-        <button onClick={logOut}>logout</button>
-      </h1>
-      <Outlet />
+      <Wrapper>
+        <Menu>
+          <Link to="/">
+            <MenuItem>
+              <svg
+                fill="#6c71c4"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path d="M19.006 3.705a.75.75 0 1 0-.512-1.41L6 6.838V3a.75.75 0 0 0-.75-.75h-1.5A.75.75 0 0 0 3 3v4.93l-1.006.365a.75.75 0 0 0 .512 1.41l16.5-6Z" />
+                <path
+                  clipRule="evenodd"
+                  fillRule="evenodd"
+                  d="M3.019 11.114 18 5.667v3.421l4.006 1.457a.75.75 0 1 1-.512 1.41l-.494-.18v8.475h.75a.75.75 0 0 1 0 1.5H2.25a.75.75 0 0 1 0-1.5H3v-9.129l.019-.007ZM18 20.25v-9.566l1.5.546v9.02H18Zm-9-6a.75.75 0 0 0-.75.75v4.5c0 .414.336.75.75.75h3a.75.75 0 0 0 .75-.75V15a.75.75 0 0 0-.75-.75H9Z"
+                />
+              </svg>
+            </MenuItem>
+          </Link>
+          <Link to="/profile">
+            <MenuItem>
+              <svg
+                fill="#6c71c4"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path
+                  clipRule="evenodd"
+                  fillRule="evenodd"
+                  d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                />
+              </svg>
+            </MenuItem>
+          </Link>
+          <Link to="/issues">
+            <MenuItem>
+              <svg
+                fill="#6c71c4"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path
+                  clipRule="evenodd"
+                  fillRule="evenodd"
+                  d="M1.5 6.375c0-1.036.84-1.875 1.875-1.875h17.25c1.035 0 1.875.84 1.875 1.875v3.026a.75.75 0 0 1-.375.65 2.249 2.249 0 0 0 0 3.898.75.75 0 0 1 .375.65v3.026c0 1.035-.84 1.875-1.875 1.875H3.375A1.875 1.875 0 0 1 1.5 17.625v-3.026a.75.75 0 0 1 .374-.65 2.249 2.249 0 0 0 0-3.898.75.75 0 0 1-.374-.65V6.375Zm15-1.125a.75.75 0 0 1 .75.75v.75a.75.75 0 0 1-1.5 0V6a.75.75 0 0 1 .75-.75Zm.75 4.5a.75.75 0 0 0-1.5 0v.75a.75.75 0 0 0 1.5 0v-.75Zm-.75 3a.75.75 0 0 1 .75.75v.75a.75.75 0 0 1-1.5 0v-.75a.75.75 0 0 1 .75-.75Zm.75 4.5a.75.75 0 0 0-1.5 0V18a.75.75 0 0 0 1.5 0v-.75ZM6 12a.75.75 0 0 1 .75-.75H12a.75.75 0 0 1 0 1.5H6.75A.75.75 0 0 1 6 12Zm.75 2.25a.75.75 0 0 0 0 1.5h3a.75.75 0 0 0 0-1.5h-3Z"
+                />
+              </svg>
+            </MenuItem>
+          </Link>
+          <MenuItem className="logOut" onClick={logOut}>
+            <svg
+              fill="none"
+              stroke="#dc322f"
+              strokeWidth={1.5}
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75"
+              />
+            </svg>
+          </MenuItem>
+        </Menu>
+        <Outlet />
+      </Wrapper>
     </>
   );
 }
